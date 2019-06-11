b2ce4bc2e7579fc7ce4ac8aa186a4215ca2a9883
Mario-Sko <marioskowronek@gmx.com>
2019-06-11 09:22:01 +0100

    Add files via upload

--- public/javascript.js
+++ public/javascript.js
@@ -1,104 +1,111 @@
 
 function seeMoreButtonReview(clickid){
-var markerId = clickid.slice(13)
-console.log("worke: " + document.getElementById("reviewSeeMore" + markerId).firstElementChild.className )
+	var markerId = clickid.slice(13)
+	var arrowIdDiv = "reviewSeeMore"
+	var wrappedIdDiv = "rowgoogleRestaurantReview"
+	ToogleReview(arrowIdDiv,wrappedIdDiv, markerId )
+	}
 
-	if (document.getElementById("reviewSeeMore" + markerId).firstElementChild.className == "fas fa-arrow-up starDesignReview"){
-		document.getElementById("googleRestaurantReview" + markerId).style.height = "100px"
-		document.getElementById("googleRestaurantReview" + markerId).style.overflow = "hidden"
-		document.getElementById("reviewSeeMore" + markerId).innerHTML = '<i class="fas fa-arrow-down starDesignReview"></i>'
-
-	}else{
-		document.getElementById("googleRestaurantReview" + markerId).style.height = "auto"
-		document.getElementById("googleRestaurantReview" + markerId).style.overflow = "visible"
-		document.getElementById("reviewSeeMore" + markerId).innerHTML = '<i class="fas fa-arrow-up starDesignReview"></i>'
-		var divContainer = document.getElementById("reviewSeeMore" + markerId)
-		var clickAttr = document.createAttribute("onclick");
-		clickAttr.value = "seeMoreButtonReview(this.id)";
-		divContainer.setAttributeNode(clickAttr)
-
-
+function restaurantReview(clicked_id){    
+	var markerId =clicked_id.slice(12)
+	//a == adding restaurants
+   var whereIsTheRestaurant = "g"
+   CompletingReviewoverlay(markerId, whereIsTheRestaurant)   		
 }
 
-}
-
-
-function restaurantReview(clicked_id)
-{    
-
+function  CompletingReviewoverlay(markerId, whereIstheRestaurant){ 
+     var markerIdWithWhereIsTheRestaurant
+	var getRestaurantName     
+     if(whereIstheRestaurant == "a"){
+        	getRestaurantName = "rowrownewRestaurant"
+        	//a == added restaurants
+         markerIdWithWhereIsTheRestaurant = "a" + markerId
+     }else{
+     		getRestaurantName = "roweachRestaurant"
+        	// g == restaurants from google api
+         markerIdWithWhereIsTheRestaurant = "g" + markerId
+     }
+     
 		if(document.getElementById("reviewWritingOverlay").firstChild != null){
 			document.getElementById("reviewWritingOverlay").firstChild.remove()
 		}
     	document.getElementById("overlayBackground").style.display = "block"
-   
-	 	var markerId = clicked_id.slice(12)
-		var restaurantName = document.getElementById("rowGoogleRestaurant"+markerId).childNodes[4].innerHTML
-		console.log(restaurantName)
+    	document.getElementById("overlay").style.display = "block"
+    	document.getElementById("mainOverlayBackground").style.display = "block"
+   	 	
+		var NameOfRestaurant = document.getElementById(getRestaurantName + markerId).childNodes[4].innerHTML
+		
 		//create div to get the restaurant name and the id of the restaurant    	
     	var divContainer = document.createElement("div")
 		var divAttr = document.createAttribute("id");
 		var classAttr = document.createAttribute("class");
-		divAttr.value = "writeReview" + markerId;
+		divAttr.value = "writeReview" + markerIdWithWhereIsTheRestaurant;
 		classAttr.value = "titleOverlay";	
 	 	divContainer.setAttributeNode(divAttr)
 		divContainer.setAttributeNode(classAttr)
-		divContainer.innerHTML = "Write a Review for " + restaurantName
+		divContainer.innerHTML = "Write a Review for " + NameOfRestaurant
 		var nodePath = document.getElementById("reviewWritingOverlay")
 		nodePath.appendChild(divContainer)	
-		
-		
-		
-}
-
+}		
 
 function createStars(clicked_id){
 	
-	
-	
 	var i = 0
-	console.log("data: "+ starData + " Name :" + starName)
 	for(i = 0; i < starData ; i++){
 		document.getElementById([i+1]+starName).style.color = "#f2ab00";
 	}
 
-	
 	for(i+1; i < 5; i++){
 		document.getElementById([i+1]+starName).style.color = "#2e2b0c";
-	
 	}
 
 }
 
 function googleSubmit(){
-    	document.getElementById("overlayBackground").style.display = "none"
-		    	
+    	document.getElementById("overlayBackground").style.display = "none"		    	
 }
+
 function googleCancel(){
 	
 	for(i = 1; i <= starData; i++){
 		document.getElementById([i]+starName).style.color = "#2e2b0c";
 	}    	
-		document.getElementById("textReview").innerHTML = "";   	
-    	
-    	
-    	document.getElementById("overlayBackground").style.display = "none"
-    	
-    	
+	document.getElementById("textReview").innerHTML = "";   	
+   document.getElementById("overlayBackground").style.display = "none" 	
 	starData = ""
 }
 
-/*
-document.getElementById("buttonGoogleCancel").addEventListener("click", function (){
-	
-//	for(i = 1; i <= starData; i++){
-//		document.getElementById([i]+starName).style.color = "#2e2b0c";
-//	}    	
+function ToogleReview(arrowIdDiv,wrappedIdDiv, markerId ){
+
+	if (document.getElementById(arrowIdDiv + markerId).firstElementChild.className == "fas fa-arrow-up starDesignReview"){
 		
-		//document.getElementById("textReview").innerHTML = "";   	
-    	
-    	
-    	document.getElementById("overlayBackground").style.display = "none"
-    	
-    	
-//	starData = ""
-})*/
+		document.getElementById(wrappedIdDiv + markerId).style.height = "100px"
+		document.getElementById(wrappedIdDiv + markerId).style.overflow = "hidden"
+		document.getElementById(arrowIdDiv  + markerId).innerHTML = '<i class="fas fa-arrow-down starDesignReview"></i>'
+	}else{
+		console.log("zwei")
+		document.getElementById(wrappedIdDiv+ markerId).style.height = "auto"
+		document.getElementById(wrappedIdDiv + markerId).style.overflow = "visible"
+		document.getElementById(arrowIdDiv + markerId).innerHTML = '<i class="fas fa-arrow-up starDesignReview"></i>'
+
+	}
+}	
+
+function seeMoreButtonAddingReview(clickid){
+	var markerId = clickid.slice(24)
+	var arrowIdDiv = "arrowAddingreviewSeeMore" 
+	var wrappedIdDiv = "reviewNewRestaurant" 
+
+	ToogleReview(arrowIdDiv,wrappedIdDiv, markerId )
+
+	}
+
+function addingRestaurantReview(clicked_id) {    
+
+	var markerId = clicked_id.slice(18)
+	//a == adding restaurants
+   var whereIsTheRestaurant = "a"
+      
+   CompletingReviewoverlay(markerId, whereIsTheRestaurant)
+		
+}
